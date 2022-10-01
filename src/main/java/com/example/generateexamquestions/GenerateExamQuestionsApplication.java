11d9e6121c415ff294a25da6e2fed0ebdf3b42c5
Alex <gt-star@yandex.ru>
2022-10-01 12:41:16 +0700

    Initial commit

---
+++ src/main/java/com/example/generateexamquestions/GenerateExamQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.generateexamquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateExamQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateExamQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/generateexamquestions/GenerateExamQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.generateexamquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenerateExamQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
